8a5487b69846d077db251f8b3fc2ccf52b570f2d
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-08-24 21:02:01 +0530

    feat: added currentTab property to NavBar

--- src/core/NavBar.tsx
+++ src/core/NavBar.tsx
@@ -1,30 +1,40 @@
 import React from 'react'
 import { Link, withRouter } from "react-router-dom";
 
-const NavBar = () => (
+const currentTab = (history, path) => {
+  if(history.location.pathname === path){
+    return {color: "#2ecc72"}
+  } else {
+    return {color: "#FFFFFF"}
+  }
+}
+
+const NavBar = ({history}) => (
     <div>
       <ul className="nav nav-tabs bg-dark">
         <li className="nav-item">
-          <Link className="nav-link" to="/">Home</Link>
+          <Link style={currentTab(history, "/")} className="nav-link" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/">Cart</Link>
+          <Link style={currentTab(history, "/cart")} className="nav-link" to="/cart">Cart</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/">Admin Dashboard</Link>
+          <Link style={currentTab(history, "/user/dashboard")} className="nav-link" to="/user/dashboard">Dashboard</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/">Signup</Link>
+          <Link style={currentTab(history, "/admin/dashboard")} className="nav-link" to="/admin/dashboard">Admin Dashboard</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/">Sign in</Link>
+          <Link style={currentTab(history, "/signup")} className="nav-link" to="/signup">Signup</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/">Sign out</Link>
+          <Link style={currentTab(history, "/signin")} className="nav-link" to="/signin">Sign in</Link>
+        </li>
+        <li className="nav-item">
+          <Link style={currentTab(history, "/signout")} className="nav-link" to="/signout">Sign out</Link>
         </li>
       </ul>
     </div>
-  )
-
+  );
 
 export default withRouter(NavBar);
