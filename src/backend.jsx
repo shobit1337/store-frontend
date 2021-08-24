e2768bb99aa03c276525416a77640cceeaff1ba1
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-08-25 01:14:59 +0530

    feat: added signup functionality

--- src/backend.jsx
+++ src/backend.jsx
@@ -1,1 +1,1 @@
-export const API = process.env.REACT_APP_BACKEND;
+export const API = import.meta.env.VITE_APP_BACKEND;
--- src/user/Signup.jsx
+++ src/user/Signup.jsx
@@ -1,8 +1,45 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { signup } from "../auth/helper";
 import Base from "../core/Base";
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+
+  const { name, email, password, error, success } = values;
+
+  // This is higher order functions, Vid is ont yt
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const handleCreate = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password })
+      .then((data) => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            success: true,
+          });
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
   const signUpForm = () => {
     return (
       <div className="row">
@@ -10,17 +47,34 @@ const Signup = () => {
           <form>
             <div className="form-group">
               <label className="text-light">Name</label>
-              <input className="form-control" type="text" />
+              <input
+                onChange={handleChange("name")}
+                className="form-control"
+                type="text"
+              />
             </div>
             <div className="form-group">
               <label className="text-light">E-mail</label>
-              <input className="form-control" type="text" />
+              <input
+                onChange={handleChange("email")}
+                className="form-control"
+                type="text"
+              />
             </div>
             <div className="form-group">
               <label className="text-light">Password</label>
-              <input className="form-control" type="password" />
+              <input
+                onChange={handleChange("password")}
+                className="form-control"
+                type="password"
+              />
             </div>
-            <button className="btn btn-success btn-block">Create</button>
+            <button
+              onClick={handleCreate}
+              className="btn btn-success btn-block"
+            >
+              Create
+            </button>
           </form>
         </div>
       </div>
