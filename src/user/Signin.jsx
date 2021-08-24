34d6b9b5c7d0b535798bbeb649f301146a11dee0
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-08-24 21:26:49 +0530

    feat: signup and signin page design

--- src/user/Signin.jsx
+++ src/user/Signin.jsx
@@ -3,9 +3,29 @@ import { Link } from "react-router-dom";
 import Base from "../core/Base";
 
 const Signin = () => {
+  const signInForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">E-mail</label>
+              <input className="form-control" type="text" />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input className="form-control" type="password" />
+            </div>
+            <button className="btn btn-success btn-block">Create</button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <Base title="Sign in page" description="A page for use to sign in!">
-      <h1>Sign in works</h1>
+      {signInForm()}
     </Base>
   );
 };
--- src/user/Signup.jsx
+++ src/user/Signup.jsx
@@ -3,9 +3,33 @@ import { Link } from "react-router-dom";
 import Base from "../core/Base";
 
 const Signup = () => {
+  const signUpForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Name</label>
+              <input className="form-control" type="text" />
+            </div>
+            <div className="form-group">
+              <label className="text-light">E-mail</label>
+              <input className="form-control" type="text" />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input className="form-control" type="password" />
+            </div>
+            <button className="btn btn-success btn-block">Create</button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <Base title="Sign up page" description="A page for use to sign up!">
-      <h1>Sign up works</h1>
+      {signUpForm()}
     </Base>
   );
 };
