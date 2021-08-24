6c88beda5d4605d7089ca1f09af306d496d6e2f8
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-08-24 22:28:05 +0530

    feat: authentication helper methods added

---
+++ src/auth/helper/index.jsx
@@ -0,0 +1,69 @@
+import { API } from "../../backend";
+
+export const signup = (user) => {
+  return fetch(`${API}/signup`, {
+    method: "POST",
+    header: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.error(err));
+};
+
+export const signin = (user) => {
+  return fetch(`${API}/signin`, {
+    method: "POST",
+    header: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.error(err));
+};
+
+export const authenticate = (data, next) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(data));
+    next();
+  }
+};
+
+export const signout = (next) => {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("jwt");
+    next();
+
+    fetch(`${API}/signout`, {
+      method: "GET",
+      header: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => {
+        return console.log("Signout success!");
+      })
+      .catch((err) => console.error(err));
+  }
+};
+
+export const isAuthenticated = () => {
+  if (typeof window == "undefined") {
+    return false;
+  }
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
+};
