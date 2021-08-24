06891744051a46b43bccf0a6647498462bcb8c90
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-08-24 21:12:39 +0530

    feat: added signin and signup routes

---
+++ src/user/Signin.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Base from "../core/Base";
+
+const Signin = () => {
+  return (
+    <Base title="Sign in page" description="A page for use to sign in!">
+      <h1>Sign in works</h1>
+    </Base>
+  );
+};
+export default Signin;
---
+++ src/user/Signup.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Base from "../core/Base";
+
+const Signup = () => {
+  return (
+    <Base title="Sign up page" description="A page for use to sign up!">
+      <h1>Sign up works</h1>
+    </Base>
+  );
+};
+export default Signup;
