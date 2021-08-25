08f1ef5a1b834f97e3f18a9329371eb71fb37189
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-08-26 02:16:47 +0530

    feat: AddCategory component

---
+++ src/admin/AddCategory.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { isAuthenticated } from "../auth/helper";
+import Base from "../core/Base";
+
+function AddCategory() {
+  const [name, setName] = useState("");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const { user, token } = isAuthenticated();
+
+  const goBackBtn = () => (
+    <div className="mt-5">
+      <Link className="btn btn-sm btn-dark mb-3" to="/admin/dashboard">
+        Back
+      </Link>
+    </div>
+  );
+
+  const categoryForm = () => (
+    <form>
+      <div className="form-group">
+        <p className="lead">Enter the category name:</p>
+        <input
+          type="text"
+          className="form-control my-3"
+          autoFocus
+          required
+          placeholder="Eg: Summer"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button className="btn btn-outline-info">Create</button>
+      </div>
+    </form>
+  );
+
+  return (
+    <Base
+      title="Create New Category"
+      description="Add a new category for new items"
+      className="container bg-info p-4"
+    >
+      <div className="row bg-white rounded">
+        <div className="col-md-8 offset-md-2">
+          {categoryForm()}
+          {goBackBtn()}
+        </div>
+      </div>
+    </Base>
+  );
+}
+
+export default AddCategory;
