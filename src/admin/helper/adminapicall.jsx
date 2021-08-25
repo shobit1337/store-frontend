7655b2a697f7a6924dc8c115c6f8cbd42c112475
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-08-26 02:17:14 +0530

    feat: createCategory api call method

---
+++ src/admin/helper/adminapicall.jsx
@@ -0,0 +1,15 @@
+import { API } from "../../backend";
+
+export const createCategopry = (userId, token, category) => {
+  return fetch(`${API}/category/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(category),
+  })
+    .then((response) => response.json())
+    .catch((err) => console.error(err));
+};
