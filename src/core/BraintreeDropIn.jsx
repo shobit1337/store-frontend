7385a51efa06c8dd1c259bd35f168e566addf21a
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-09-01 03:17:41 +0530

    feat: braintree payment integration

---
+++ src/core/BraintreeDropIn.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { emptyCart, loadCart } from "./helper/cartHelper";
+import { fetchToken, processPayment } from "./helper/paymentBraintreeHelper";
+import { createOrder } from "./helper/orderHelper";
+import { isAuthenticated } from "../auth/helper";
+import DropIn from "braintree-web-drop-in-react";
+
+const BraintreeDropIn = ({
+  products,
+  setTiggerReload = (x) => x,
+  tiggerReload = undefined,
+}) => {
+  const [info, setInfo] = useState({
+    loading: false,
+    success: false,
+    clientToken: null,
+    instance: {},
+    error: "",
+  });
+
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+  const token = isAuthenticated() && isAuthenticated().token;
+
+  const getFinalAmount = (products) => {
+    return products.reduce((currentValue, nextValue) => {
+      return currentValue + nextValue.count * nextValue.price;
+    }, 0);
+  };
+
+  const getToken = (userId, token) => {
+    fetchToken(userId, token).then((data) => {
+      if (data.error) {
+        setInfo({ ...info, error: data.error });
+      } else {
+        let clientToken = data.clientToken;
+        setInfo({ clientToken });
+      }
+    });
+  };
+
+  const showDropIn = () => {
+    return (
+      <div>
+        {info.clientToken !== null && products.length > 0 ? (
+          <div>
+            <DropIn
+              options={{ authorization: info.clientToken }}
+              onInstance={(instance) => (info.instance = instance)}
+            />
+            <button className="btn btn-block btn-success" onClick={onPurchase}>
+              Buy
+            </button>
+          </div>
+        ) : (
+          <h3>Login and add items to continue</h3>
+        )}
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    getToken(userId, token);
+  }, []);
+
+  const onPurchase = () => {
+    setInfo({ loading: true });
+    let nonce;
+    let getNonce = info.instance.requestPaymentMethod().then((data) => {
+      nonce = data.nonce;
+      let paymentData = {
+        paymentMethodNonce: nonce,
+        amount: getFinalAmount(products),
+      };
+      processPayment(userId, token, paymentData)
+        .then((data) => {
+          setInfo({ ...info, success: true, loading: false });
+        })
+        .catch((err) =>
+          setInfo({ ...info, error: err, loading: false, success: false })
+        );
+    });
+  };
+
+  return (
+    <div>
+      <h1>{showDropIn()}</h1>
+      {info.success && <h2 className="text-success">Payment Successfull!</h2>}
+      {info.error && <h2 className="text-warning">Payment Failed.</h2>}
+    </div>
+  );
+};
+
+export default BraintreeDropIn;
--- src/core/Cart.jsx
+++ src/core/Cart.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "../styles.css";
 import Base from "./Base";
+import BraintreeDropIn from "./BraintreeDropIn";
 import Card from "./Card";
 import { loadCart } from "./helper/cartHelper";
-import StripeCheckout from "./StripeCheckout";
+// import StripeCheckout from "./StripeCheckout";
 
 const Cart = () => {
   const [products, setProducts] = useState([]);
@@ -13,7 +14,7 @@ const Cart = () => {
     setProducts(loadCart());
   }, [tiggerReload]);
 
-  const loadAllProducts = () => {
+  const loadAllProducts = (products) => {
     return (
       <div>
         <h2>Cart Items:</h2>
@@ -37,7 +38,12 @@ const Cart = () => {
     return (
       <div>
         <h2>Checkout:</h2>
-        <StripeCheckout
+        {/* <StripeCheckout
+          products={products}
+          setTiggerReload={setTiggerReload}
+          tiggerReload={tiggerReload}
+        /> */}
+        <BraintreeDropIn
           products={products}
           setTiggerReload={setTiggerReload}
           tiggerReload={tiggerReload}
@@ -49,7 +55,13 @@ const Cart = () => {
   return (
     <Base title="My Cart" description="Ready to checkout!">
       <div className="row">
-        <div className="col-6">{loadAllProducts()}</div>
+        <div className="col-6">
+          {products.length > 0 ? (
+            loadAllProducts(products)
+          ) : (
+            <h3>Your cart is empty :(</h3>
+          )}
+        </div>
         <div className="col-6">{loadCheckout()}</div>
       </div>
     </Base>
